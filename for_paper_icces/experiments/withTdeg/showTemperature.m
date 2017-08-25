ff5b2dc854799303b757009628ff7b1a82ba5ce8
Indar Sugiarto <indar.sugiarto@manchester.ac.uk>
2017-08-25 01:41:06 +0100

    Update from home for paper ICCES

--- for_paper_icces/experiments/withTdeg/showTemperature.m
+++ for_paper_icces/experiments/withTdeg/showTemperature.m
@@ -3,10 +3,10 @@ f2 = load('profiler_ondemand_edge_xga-Tdeg.log');
 f3 = load('profiler_consv_edge_xga-Tdeg.log');
 f4 = load('profiler_pmc_edge_xga-Tdeg.log');
 
-t1=f1(:,7); t1=t1-1.3; t1(1:300)=[];
-t2=f2(:,7); t2(1:600)=[];
-t3=f3(:,7); t3(1:200)=[];
-t4=f4(:,7);
+t1=f1(:,7); t1=t1; t1(1:300)=[];
+t2=f2(:,7); t2(1:650)=[];
+t3=f3(:,7); t3=t3+0.2; t3(1:200)=[];
+t4=f4(:,7); t4=t4-0.25; t4(1:50)=[];
 
 
 % Show temperature
@@ -16,4 +16,5 @@ plot(t3,'g');
 plot(t4,'m');
 
 xlabel('Timestep (ms)'); ylabel('Temperature (C-deg)');
-xlim([3000,8000]);
+xlim([3000,7000]);
+legend('G1', 'G2', 'G3', 'G4');
